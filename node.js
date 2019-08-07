b66435a598810c596390aaaee065952ae75ba0b7
yelamarthy1 <53737396+yelamarthy1@users.noreply.github.com>
2019-08-07 15:04:23 +0530

    Create node.js

---
+++ node.js
@@ -0,0 +1,27 @@
+/*
+npm search express
+RUN npm install -y express --save
+npm install body-parser --save
+npm install cookie-parser --save
+npm install multer --save
+*/
+
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "data.json", 'utf8', function (err, data) {
+       console.log( data.toString() );
+       res.end( data );
+   });
+})
+
+var server = app.listen(9000, "0.0.0.0",function () {
+
+  var host = server.address().address;
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s",host, port)
+
+})
